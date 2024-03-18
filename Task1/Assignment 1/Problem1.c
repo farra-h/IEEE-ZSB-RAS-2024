b66fcb48b27748b9caa3430043e7ef5ef5eec3a2
Farah Mahmoud <134429241+farra-h@users.noreply.github.com>
2024-03-19 00:34:18 +0200

    Update Problem1.c

--- Task1/Assignment 1/Problem1.c
+++ Task1/Assignment 1/Problem1.c
@@ -6,12 +6,18 @@ int main(){
     printf("Enter the size of the array: ");
     scanf("%d", &size);
 
-    int arr[size], seen[size];
+    int arr[size];
     int sum = 0;
-
     printf("Enter the elements of the array: ");
+    int max_element = 0;
     for(int i=0; i<size; i++){
         scanf("%d", &arr[i]);
+        if(arr[i] > max_element)
+            max_element = arr[i];
+    }
+
+    int seen[max_element + 1];
+    for(int i=0; i<=max_element; i++){
         seen[i] = 0;
     }
 
@@ -19,7 +25,7 @@ int main(){
         seen[arr[i]]++;
     }
 
-    for(int i=0; i<size; i++){
+    for(int i=0; i<=max_element; i++){
         if(seen[i] > 1)
             sum++;
     }
