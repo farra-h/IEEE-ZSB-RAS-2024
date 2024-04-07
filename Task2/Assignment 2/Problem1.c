8e0f2d4175b896cccb655d689ffbb30d86fba73c
Farah Mahmoud <faraheltahawy27@gmail.com>
2024-04-07 15:53:37 +0200

    problem 1

--- Task2/Assignment 2/Problem1.c
+++ Task2/Assignment 2/Problem1.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int *increment(int *value){
+    (*value)++;
+    return value;
+}
+
+int main(){
+    int value;
+    printf("Enter the number you want to increment: ");
+    scanf("%d", &value);
+
+    int *ptr;
+    ptr = increment(&value);
+    printf("The incremented value is: %d", *ptr);
+
+    return 0;
+}
