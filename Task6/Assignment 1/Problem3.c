102974e2689ff714df9f16f520cd73d759243056
Farah Mahmoud <faraheltahawy27@gmail.com>
2024-05-02 21:40:39 +0300

    assignment 1 prb 3

--- Task6/Assignment 1/Problem3.c
+++ Task6/Assignment 1/Problem3.c
@@ -4,15 +4,13 @@ int maxFreq(char arr[], int size){
 
     int freq[256] = {0};
     int max = -1;
-    //char result;
 
-    for(int i = 0; i < size; i++)
-        freq[arr[i]]++;
+    for(int i = 0; i < size; i++) // i=0
+        freq[arr[i]]++;  // freq[arr[0]]  freq[first char ascii] ++ 0 >> 1
 
     for(int i = 0; i < size; i++){
         if(max < freq[arr[i]]){
             max = freq[arr[i]];
-            //result = arr[i];
         }
     }
 
@@ -27,7 +25,7 @@ int main(){
 
     char arr[size+1];
 
-    getchar();
+    getchar(); // consume the newline character (\n) that is left in the input buffer after scanf, without it it didn't give me the output i want
 
     printf("Enter the name: ");
     for (int i = 0; i < size; i++)
